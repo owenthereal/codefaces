d7cf98533c3a67bf7658537a59135ad833eb4d2d
KK Lo <lokwunkit@gmail.com>
2010-05-28 16:27:09 -0700

    added a new theme

---
+++ org.codefaces.ui.themes/private/azure/config.rb
@@ -0,0 +1,9 @@
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = "../../themes/azure/"
+css_dir = "../../themes/azure/stylesheet"
+sass_dir = "src"
+images_dir = "../../themes/azure/images"
+javascripts_dir = "javascripts"
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
