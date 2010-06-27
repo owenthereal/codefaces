9339ebe5a2bb093943d28066a06a25f69b095749
KK Lo <lokwunkit@gmail.com>
2010-06-26 19:50:39 -0700

    Clean up

--- org.codefaces.ui/src/org/codefaces/ui/commands/AboutCommandHandler.java
+++ org.codefaces.ui/src/org/codefaces/ui/commands/AboutCommandHandler.java
@@ -9,17 +9,8 @@ import org.eclipse.ui.handlers.HandlerUtil;
 
 public class AboutCommandHandler extends AbstractHandler {
 
-	private static final String TITLE = "About CodeFaces";
-
-	private static final String MESSAGE = "CodeFaces is a source control client for browsers" +
-		"based on Ajax RIA technology. We are currently in beta and looking for your feedback. " +
-		"To contact us, you can join our community forums or send us an email.\n\n" + 
-		"- The CodeFaces Team";
-
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		//MessageDialog.openInformation(PlatformUI.getWorkbench()
-		//		.getActiveWorkbenchWindow().getShell(), TITLE, MESSAGE);
 		Shell shell = HandlerUtil.getActiveShell(event);
 		new AboutDialog(shell).open();
 		return null;
