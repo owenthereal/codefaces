25cfdc70a1613174dc2ae4edcc8654d711820bda
KK Lo <lokwunkit@gmail.com>
2010-07-13 22:56:46 -0700

    Igores SvnUtil test.

--- org.codefaces.core.tests/src/org/codefaces/core/services/svn/SvnUtilTest.java
+++ org.codefaces.core.tests/src/org/codefaces/core/services/svn/SvnUtilTest.java
@@ -1,12 +1,15 @@
 package org.codefaces.core.services.svn;
 
 
+import org.junit.Ignore;
+
 import junit.framework.TestCase;
 
 /**
  * This test case have to be run in RAP test which only supports JUnit3
  * click Run As -> RAP JUnit Test
  */
+@Ignore
 public class SvnUtilTest extends TestCase{
 
 	public void test_ClientAdaptorIsNotNull(){
