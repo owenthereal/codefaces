d297fc94a97e9adf672a8de5d8af77650e296324
KK Lo <lokwunkit@gmail.com>
2010-07-15 20:17:21 -0700

    new version for branch operation test

--- org.codefaces.core.svn.tests/src/org/codefaces/core/svn/operations/SvnFetchBranchesQueryTest.java
+++ org.codefaces.core.svn.tests/src/org/codefaces/core/svn/operations/SvnFetchBranchesQueryTest.java
@@ -1,61 +1,49 @@
-package org.codefaces.core.services.svn;
+package org.codefaces.core.svn.operations;
+
+import static org.junit.Assert.*;
 
 import java.net.MalformedURLException;
 import java.util.Collection;
-import java.util.Date;
 
+import org.codefaces.core.connectors.SCMConnector;
 import org.codefaces.core.models.Repo;
 import org.codefaces.core.models.RepoBranch;
 import org.codefaces.core.models.RepoCredential;
+import org.codefaces.core.operations.SCMOperationHandler;
+import org.codefaces.core.operations.SCMOperationParameters;
 import org.codefaces.core.svn.operations.SVNConstants;
 import org.codefaces.core.svn.operations.SVNFetchBranchesOperationHandler;
-import org.tigris.subversion.svnclientadapter.ISVNClientAdapter;
-import org.tigris.subversion.svnclientadapter.ISVNDirEntry;
-import org.tigris.subversion.svnclientadapter.SVNClientException;
-import org.tigris.subversion.svnclientadapter.SVNResourceKind;
-import org.tigris.subversion.svnclientadapter.SVNRevision;
-import org.tigris.subversion.svnclientadapter.SVNUrl;
+import org.junit.Before;
+import org.junit.Test;
 
 public class SvnFetchBranchesQueryTest {
 	
 	private static String TEST_URL_WITHOUT_BRANCHES = "http://code.djangoproject.com/svn/django/trunk";
 	private static String TEST_URL_WITH_BRANCHES = "http://code.djangoproject.com/svn/django";
 	
-	class MockSvnFetchBrancheQuery extends SVNFetchBranchesOperationHandler {
-		private ISVNClientAdapter testClient;
-		
-		public void setClient(ISVNClientAdapter testClient){
-			this.testClient = testClient;
-		}
-		
-		@Override
-		protected ISVNClientAdapter getSvnClient(){
-			return testClient == null? new MockSVNClientAdaptor() : testClient;
-		}
-	}
+	private static String TEST_BRAHCH_NAME = "0.96-bugfixes";
 	
-	// Create a mock Repo object for testing
-	private Repo createModRepo(String url, String username, String password)
-			throws MalformedURLException {
-		SVNUrl svnUrl = new SVNUrl(url);
-		RepoCredential credential = new RepoCredential(null, username, password);
-		return new Repo(svnUrl.toString(), svnUrl.toString(), credential);
-	}
-	
-	private MockSvnFetchBrancheQuery query;
-	
+	private SVNFetchBranchesOperationHandler handler;
+	private SCMConnector connector;
+
 	@Before
 	public void setUp(){
-		query = new MockSvnFetchBrancheQuery();
+		connector = new MockSCMConnector(TestSvnJavaHlClientAdaptor.getClient());
+		handler = new SVNFetchBranchesOperationHandler();
+	}
+	
+	private Repo createMockRepo(String url, String username, String password){
+		RepoCredential credential = new RepoCredential(null, username, password);
+		return new Repo(null, url, url, credential);
 	}
 	
 	@Test
-	public void directoriesWithoutBranchesFolderShouldOnlyContainsDefaultBranch()
-			throws MalformedURLException {
-		SCMQueryParameter para = SCMQueryParameter.newInstance();
-		Repo repo = createModRepo(TEST_URL_WITHOUT_BRANCHES, null, null);
-		para.addParameter(SCMQuery.PARA_REPO, repo);
-		Collection<RepoBranch> branches = query.execute(null, para);
+	public void directoriesWithoutBranchesFolderShouldOnlyContainsDefaultBranch(){
+		SCMOperationParameters para = SCMOperationParameters.newInstance();
+		Repo repo = createMockRepo(TEST_URL_WITHOUT_BRANCHES, null, null);
+		para.addParameter(SCMOperationHandler.PARA_REPO, repo);
+		
+		Collection<RepoBranch> branches = handler.execute(connector, para);
 		
 		assertEquals(1, branches.size());
 		
@@ -63,54 +51,30 @@ public class SvnFetchBranchesQueryTest {
 		assertEquals(SVNConstants.DEFAULT_BRANCH, defaultBranch.getName());
 	}
 		
+	
 	@Test
 	public void directoriesInsideBranchesFolderShouldBeReturnedWhenThereIsAFolderCalledBranches()
 			throws MalformedURLException{
+		SCMOperationParameters para = SCMOperationParameters.newInstance();
+		Repo repo = createMockRepo(TEST_URL_WITH_BRANCHES, null, null);
+		para.addParameter(SCMOperationHandler.PARA_REPO, repo);
+		Collection<RepoBranch> allBranches = handler.execute(connector, para);
 		
-		//In this test, we create a mock SVN client. If a branches directory is queried, it
-		//shows up one file and one folder
-		final MockSVNDirEntry branchInBranchesDir = new MockSVNDirEntry();
-		final MockSVNDirEntry fileInBranchesDir = new MockSVNDirEntry();
-		branchInBranchesDir.setPath("branchInBranchesDir");
-		fileInBranchesDir.setPath("fileInBranchesDir");
-		branchInBranchesDir.setNodeKind(SVNResourceKind.DIR);
-		fileInBranchesDir.setNodeKind(SVNResourceKind.FILE);
-		branchInBranchesDir.setLastChangedDate(new Date());
-		fileInBranchesDir.setLastChangedDate(new Date());
-		
-		query.setClient(new MockSVNClientAdaptor(){
-			@Override
-			public ISVNDirEntry[] getList(SVNUrl url, SVNRevision revision,
-					boolean recurse) throws SVNClientException {
-				return getList(url, revision, null, recurse);
+		//TEST_BRANCH_NAME should be one of the branches
+		//also for defaultBranch
+		boolean branchFound = false;
+		boolean defaultBranchFound = false;
+		for(RepoBranch branch: allBranches){
+			if(branch.getName().equals(TEST_BRAHCH_NAME)){
+				branchFound = true;
+				assertTrue(branch.isMaster() == false);
 			}
-			
-			@Override
-			public ISVNDirEntry[] getList(SVNUrl url, SVNRevision revision,
-					SVNRevision pegRevision, boolean recurse)
-					throws SVNClientException {
-				if(url.toString().endsWith(SVNConstants.BRANCH_DIRECTORY)){
-					return  new  ISVNDirEntry[]{branchInBranchesDir, fileInBranchesDir};
-				}
-				else{
-					return new  ISVNDirEntry[]{};
-				}
+			else if(branch.getName().equals(SVNConstants.DEFAULT_BRANCH)){
+				defaultBranchFound = true;
+				assertTrue(branch.isMaster() == true);
 			}
-		});
-		
-		SCMQueryParameter para = SCMQueryParameter.newInstance();
-		Repo repo = createModRepo(TEST_URL_WITH_BRANCHES, null, null);
-		para.addParameter(SCMQuery.PARA_REPO, repo);
-		Collection<RepoBranch> branches = query.execute(null, para);
-		
-		//there should be one default branch and one branch in the Branches folder
-		assertEquals(2, branches.size());
-		RepoBranch[] branchesArray = branches.toArray(new RepoBranch[]{});
-		assertTrue(branchesArray[0].getName() == branchInBranchesDir.getPath()
-				|| branchesArray[1].getName() == branchInBranchesDir.getPath());
-		assertTrue(branchesArray[0].getName() == SVNConstants.DEFAULT_BRANCH
-				|| branchesArray[1].getName() == SVNConstants.DEFAULT_BRANCH);
+		}
+		assertTrue(branchFound);
+		assertTrue(defaultBranchFound);
 	}
-	
-
 }
