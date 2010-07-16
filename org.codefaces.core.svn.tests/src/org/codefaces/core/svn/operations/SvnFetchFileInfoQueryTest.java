34e34e5242ea120ee5edde061aac416a4d674193
KK Lo <lokwunkit@gmail.com>
2010-07-15 22:22:01 -0700

    new version of test for fetch file info

--- org.codefaces.core.svn.tests/src/org/codefaces/core/svn/operations/SvnFetchFileInfoQueryTest.java
+++ org.codefaces.core.svn.tests/src/org/codefaces/core/svn/operations/SvnFetchFileInfoQueryTest.java
@@ -2,68 +2,85 @@ package org.codefaces.core.svn.operations;
 
 import static org.junit.Assert.*;
 
+import org.codefaces.core.connectors.SCMConnector;
 import org.codefaces.core.models.Repo;
 import org.codefaces.core.models.RepoBranch;
 import org.codefaces.core.models.RepoCredential;
 import org.codefaces.core.models.RepoFile;
 import org.codefaces.core.models.RepoFileInfo;
-import org.codefaces.core.services.SCMQuery;
-import org.codefaces.core.services.SCMQueryParameter;
-import org.codefaces.core.services.svn.MockSVNClientAdaptor;
-import org.codefaces.core.services.svn.TestSvnJavaHlClientAdaptor;
+import org.codefaces.core.operations.SCMOperationHandler;
+import org.codefaces.core.operations.SCMOperationParameters;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+
 import org.junit.Before;
 import org.junit.Test;
-import org.tigris.subversion.svnclientadapter.ISVNClientAdapter;
 
 public class SvnFetchFileInfoQueryTest {
-	private static final String TEST_URL_WITHOUT_BRANCHES =
+	
+	private static String TEST_URL_WITHOUT_BRANCHES = 
 		"http://code.djangoproject.com/svn/django/trunk";
-	private static final String TEST_URL_WITH_BRANCHES =
-		"http://subclipse.tigris.org/svn/subclipse";
+	private static String TEST_URL_WITH_BRANCHES = 
+		"http://code.djangoproject.com/svn/django";
 	private static final String TEST_USERNAME =  null;
 	private static final String TEST_PASSWORD = null;
-	private static final String TEST_FILENAME = "README";
 	
-	class MockSvnFetchFileInfoQuery extends SvnFetchFileInfoQuery{
-		private ISVNClientAdapter testClient;
+	private static final String TEST_FILENAME_IN_TRUNK = "README";
+	private static final String TEST_FILEPATH_IN_TRUNK = "/README";
+	private static final String TEST_PREFIX_FOR_TESTFILE_IN_TRUNK = "Django";
+	
+	private static final String TEST_BRANCH = "0.96-bugfixes";
+	private static final String TEST_FILENAME_IN_BRANCH = "MANIFEST.in";
+	private static final String TEST_FILEPATH_IN_BRANCH = "/MANIFEST.in";
+	private static final String TEST_PREFIX_FOR_TESTFILE_IN_BRANCH = "include AUTHORS";
+	
+	private SVNFetchFileInfoOperationHandler handler;
+	private SCMConnector connector;
 
-		public void setClient(ISVNClientAdapter testClient){
-			this.testClient = testClient;
-		}
-		
-		@Override
-		protected ISVNClientAdapter getSvnClient(){
-			return testClient == null? new MockSVNClientAdaptor() : testClient;
-		}
-	}
-	
-	private MockSvnFetchFileInfoQuery query;
-	
 	@Before
-	public void setup(){
-		query = new MockSvnFetchFileInfoQuery();
+	public void setUp(){
+		connector = new MockSCMConnector(TestSvnJavaHlClientAdaptor.getClient());
+		handler = new SVNFetchFileInfoOperationHandler();
 	}
 	
+	
+	private RepoFile createMockFile(String repoUrl, String username,
+			String password, boolean inMasterBranch, String branchName,
+			String fileName, final String fileFullPath) {
+		RepoCredential credential = new RepoCredential(null, username, password);
+		Repo repo = new Repo(null, repoUrl, repoUrl, credential);
+		RepoBranch branch = new RepoBranch(repo, "branchId", branchName,
+				inMasterBranch);
+		return new RepoFile(branch.getRoot(), branch.getRoot(), "fileId", fileName){
+			@Override
+			public IPath getFullPath(){
+				return new Path(fileFullPath);
+			}
+		};
+	}
 	
 	@Test
 	public void gettingFileInDefaultBranch(){
-		RepoCredential credential = new RepoCredential(null, TEST_USERNAME, TEST_PASSWORD);
-		Repo repo = new Repo(TEST_URL_WITHOUT_BRANCHES, "repoId", credential);
-		RepoBranch defaultBranch = new RepoBranch(repo, "branchId", "default", true);
-		RepoFile file = new RepoFile(defaultBranch.getRoot(), defaultBranch.getRoot(),
-				"fileId",  TEST_FILENAME);
-		SCMQueryParameter para = SCMQueryParameter.newInstance();
-		para.addParameter(SCMQuery.PARA_REPO_FILE, file);
-		//using a real client is more convenience
-		query.setClient(TestSvnJavaHlClientAdaptor.getClient());
-		RepoFileInfo fileIndo = query.execute(null, para);
-		assertEquals("text", fileIndo.getMimeType());
+		RepoFile file = createMockFile(TEST_URL_WITHOUT_BRANCHES,
+				TEST_USERNAME, TEST_PASSWORD, true,
+				SVNConstants.DEFAULT_BRANCH, TEST_FILENAME_IN_TRUNK,
+				TEST_FILEPATH_IN_TRUNK);
+		SCMOperationParameters para = SCMOperationParameters.newInstance();
+		para.addParameter(SCMOperationHandler.PARA_REPO_FILE, file);
+		RepoFileInfo info = handler.execute(connector, para);
+		assertTrue(info.getContent().startsWith(TEST_PREFIX_FOR_TESTFILE_IN_TRUNK));
 	}
-	
+
 	@Test
-	public void gettingFileOtherThanDefaultBranch(){
-		throw new RuntimeException();
+	public void gettingFileInsideABranch(){
+		RepoFile file = createMockFile(TEST_URL_WITH_BRANCHES,
+				TEST_USERNAME, TEST_PASSWORD, false,
+				TEST_BRANCH, TEST_FILENAME_IN_BRANCH,
+				TEST_FILEPATH_IN_BRANCH);
+		SCMOperationParameters para = SCMOperationParameters.newInstance();
+		para.addParameter(SCMOperationHandler.PARA_REPO_FILE, file);
+		RepoFileInfo info = handler.execute(connector, para);
+		assertTrue(info.getContent().startsWith(TEST_PREFIX_FOR_TESTFILE_IN_BRANCH));
 	}
-	
 	
 }
