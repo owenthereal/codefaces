a5cec937dc27fc3e9fdcb635c89cf47d58475133
KK Lo <lokwunkit@gmail.com>
2010-07-18 18:39:47 -0700

    added a window title

--- org.codefaces.ui/src/org/codefaces/ui/internal/dialogs/ChangeRepoBaseDirectoryDialog.java
+++ org.codefaces.ui/src/org/codefaces/ui/internal/dialogs/ChangeRepoBaseDirectoryDialog.java
@@ -2,34 +2,59 @@ package org.codefaces.ui.internal.dialogs;
 
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 
 public class ChangeRepoBaseDirectoryDialog extends TitleAreaDialog{
 
-	private static final String DIALOG_LABEL_TEXT = "Select new base directory:";
+	private static final String TITLE = "Select base directory:";
+	private static final String DESCRIPTION = "Select a directory as the base directory.";
+	private static final String WINDOW_TITLE = "Select Base Directory";
+	
+	private TreeViewer viewer;
 
 	public ChangeRepoBaseDirectoryDialog(Shell parentShell) {
 		super(parentShell);
+		setShellStyle(SWT.CLOSE | SWT.MAX | SWT.TITLE | SWT.BORDER
+				| SWT.APPLICATION_MODAL | getDefaultOrientation());
 	}
 
 	@Override
 	protected Composite createDialogArea(Composite parent){
 		Composite composite = (Composite) super.createDialogArea(parent);
-		Composite dialogAreaComposite = new Composite(composite, SWT.NONE);		
-		dialogAreaComposite.setLayout(new GridLayout(2, false));
+		Composite dialogAreaComposite = new Composite(composite, SWT.NONE);
+		GridLayout layout = new GridLayout(2, false);
+		layout.marginHeight = convertVerticalDLUsToPixels(IDialogConstants.HORIZONTAL_MARGIN);
+		layout.marginWidth = convertHorizontalDLUsToPixels(IDialogConstants.VERTICAL_MARGIN);
+		layout.verticalSpacing = convertVerticalDLUsToPixels(IDialogConstants.VERTICAL_SPACING);
+		layout.horizontalSpacing = convertHorizontalDLUsToPixels(IDialogConstants.HORIZONTAL_SPACING);
+		dialogAreaComposite.setLayout(layout);
+		dialogAreaComposite.setLayoutData(new GridData(GridData.FILL_BOTH));
+		dialogAreaComposite.setFont(composite.getFont());
 
-		Label dialogLabel = new Label(dialogAreaComposite, SWT.NONE);
-		GridData diallogLabelGridData = new GridData();
-		diallogLabelGridData.horizontalSpan = 2;
-		dialogLabel.setData(diallogLabelGridData);
-		dialogLabel.setText(DIALOG_LABEL_TEXT);
+		viewer = new TreeViewer(dialogAreaComposite, SWT.NONE);
+		viewer.setInput(null);
+		
+		setTitle(TITLE);
+		setMessage(DESCRIPTION);
+		setWindowTitle(WINDOW_TITLE);
 		
 		return composite;
+	}
+	
+	/**
+	 * Set the shell window title of this dialog
+	 * @param windowTitle the window title
+	 */
+	private void setWindowTitle(String windowTitle) {
+		if (getShell() == null) {
+			return;
+		}
+		getShell().setText(windowTitle);
 	}
 	
 	@Override
