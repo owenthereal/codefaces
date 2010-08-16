3cd9bddb2f142ab10adf44c815265c6ee31d1a0d
KK Lo <lokwunkit@gmail.com>
2010-08-16 16:58:53 -0700

    move to new welcome page

--- org.codefaces.ui/src/org/codefaces/ui/internal/editors/WelcomeEditor.java
+++ org.codefaces.ui/src/org/codefaces/ui/internal/editors/WelcomeEditor.java
@@ -4,7 +4,7 @@ package org.codefaces.ui.internal.editors;
 public class WelcomeEditor extends BrowserEditor {
 	public static final String ID = "org.codefaces.ui.editor.welcomeEditor";
 
-	private static final String WELCOME_PAGE_URL = "http://codefaces.com/";
+	private static final String WELCOME_PAGE_URL = "http://blog.codefaces.org/";
 
 	public WelcomeEditor(){
 		super(WELCOME_PAGE_URL);
