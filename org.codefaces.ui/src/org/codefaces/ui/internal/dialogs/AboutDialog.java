d01b6cfac68716f006e549858837f7a75d0bab38
Jingwen Owen Ou <jingweno@gmail.com>
2010-08-18 14:32:50 -0700

    changed about dialog

--- org.codefaces.ui/src/org/codefaces/ui/internal/dialogs/AboutDialog.java
+++ org.codefaces.ui/src/org/codefaces/ui/internal/dialogs/AboutDialog.java
@@ -3,10 +3,12 @@ package org.codefaces.ui.internal.dialogs;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.codefaces.ui.internal.CodeFacesUIActivator;
 import org.codefaces.ui.internal.Images;
 import org.codefaces.ui.internal.commands.CommandExecutor;
 import org.codefaces.ui.internal.commands.OpenEditorHandler;
 import org.codefaces.ui.internal.editors.SupportEditor;
+import org.codefaces.ui.internal.editors.WelcomeEditor;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.IconAndMessageDialog;
 import org.eclipse.jface.layout.GridDataFactory;
@@ -18,56 +20,56 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Link;
 import org.eclipse.swt.widgets.Shell;
 
-
 public class AboutDialog extends IconAndMessageDialog {
-	
+
 	private static final int MESSAGE_WIDTH = 200;
-	
+
 	private static final String TITLE = "About";
 	private static final String LOGO_TOOLTIPS_TEXT = "contact us";
-	
-	private static final String MESSAGE = "CodeFaces is a source control client for browsers "
-			+ "based on Ajax RIA technology. We are currently in beta and looking for your feedback. "
-			+ "To contact us, click the icon on the left.";
-	
+
+	private static final String MESSAGE = "CodeFaces\n"
+			+ "Version: "
+			+ CodeFacesUIActivator.getDefault().getBundle().getVersion()
+					.toString()
+			+ "\n(c) Copyright CodeFaces 2010. All rights reserved. Visit <a>http://codefaces.org/</a>.";
+
 	public AboutDialog(Shell parentShell) {
 		super(parentShell);
 		setShellStyle(getShellStyle() | SWT.SHEET);
 	}
-	
+
 	@Override
-	protected void configureShell(Shell shell){
+	protected void configureShell(Shell shell) {
 		super.configureShell(shell);
 		shell.setText(TITLE);
 	}
-	
-	//create only OK button
+
 	@Override
 	protected void createButtonsForButtonBar(Composite parent) {
-		Button button = createButton(parent, IDialogConstants.OK_ID, IDialogConstants.get().OK_LABEL,
-				true);
+		Button button = createButton(parent, IDialogConstants.OK_ID,
+				IDialogConstants.get().OK_LABEL, true);
 		button.setFocus();
 	}
-	
+
 	@Override
 	protected Control createDialogArea(Composite parent) {
 		Composite composite = (Composite) super.createDialogArea(parent);
-        GridLayout layout = new GridLayout();
-        layout.marginHeight = 0;
-        layout.marginWidth = 0;
-        composite.setLayout(layout);
-		
+		GridLayout layout = new GridLayout();
+		layout.marginHeight = 0;
+		layout.marginWidth = 0;
+		composite.setLayout(layout);
+
 		Composite dialogAreaComposite = new Composite(composite, SWT.NONE);
 		GridLayout dialogArealayout = new GridLayout();
 		dialogArealayout.marginHeight = 0;
 		dialogArealayout.marginWidth = 0;
 		dialogArealayout.numColumns = 2;
 		dialogAreaComposite.setLayout(dialogArealayout);
-		
-		//create contact us button
+
+		// create contact us button
 		Image image = getImage();
 		Button contactUs = new Button(dialogAreaComposite, SWT.FLAT);
 		contactUs.setImage(image);
@@ -76,27 +78,37 @@ public class AboutDialog extends IconAndMessageDialog {
 
 		GridDataFactory.fillDefaults().align(SWT.CENTER, SWT.BEGINNING)
 				.applyTo(contactUs);
-		
+
 		contactUs.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent event) {
-				//small optimization, create a map with smaller-than-default capacity
-				Map<String, String> parameterMap = new HashMap<String, String>(4);
-				parameterMap.put(OpenEditorHandler.PARAMETER_EDITOR_ID, SupportEditor.ID);
-				CommandExecutor.execute(OpenEditorHandler.ID, parameterMap, null);
-				close();
+				openEditor(SupportEditor.ID);
 			}
 		});
-		
-		// create message
-		Label messageLabel = new Label(dialogAreaComposite, SWT.WRAP);
-		messageLabel.setText(MESSAGE);
-		GridDataFactory.fillDefaults().align(SWT.FILL, SWT.BEGINNING)
+
+		Link linkText = new Link(dialogAreaComposite, SWT.NONE);
+		linkText.setText(MESSAGE);
+		linkText.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				openEditor(WelcomeEditor.ID);
+			}
+		});
+		GridDataFactory
+				.fillDefaults()
+				.align(SWT.FILL, SWT.BEGINNING)
 				.grab(true, false)
 				.hint(convertHorizontalDLUsToPixels(MESSAGE_WIDTH), SWT.DEFAULT)
-				.applyTo(messageLabel);
-		
+				.applyTo(linkText);
+
 		return composite;
+	}
+
+	private void openEditor(String id) {
+		Map<String, String> parameterMap = new HashMap<String, String>(4);
+		parameterMap.put(OpenEditorHandler.PARAMETER_EDITOR_ID, id);
+		CommandExecutor.execute(OpenEditorHandler.ID, parameterMap, null);
+		close();
 	}
 
 	@Override
