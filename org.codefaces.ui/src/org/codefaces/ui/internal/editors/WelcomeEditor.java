e53e223f0ab4e8f2897089240babd79ebaaa99ff
Jingwen Owen Ou <jingweno@gmail.com>
2010-08-17 17:18:15 -0700

    changed google analytics code to track subdomain

--- org.codefaces.ui/src/org/codefaces/ui/internal/editors/WelcomeEditor.java
+++ org.codefaces.ui/src/org/codefaces/ui/internal/editors/WelcomeEditor.java
@@ -1,12 +1,11 @@
 package org.codefaces.ui.internal.editors;
 
-
 public class WelcomeEditor extends BrowserEditor {
 	public static final String ID = "org.codefaces.ui.editor.welcomeEditor";
 
 	private static final String WELCOME_PAGE_URL = "http://blog.codefaces.org/";
 
-	public WelcomeEditor(){
+	public WelcomeEditor() {
 		super(WELCOME_PAGE_URL);
 		setTitleToolTip("Welcome");
 	}
