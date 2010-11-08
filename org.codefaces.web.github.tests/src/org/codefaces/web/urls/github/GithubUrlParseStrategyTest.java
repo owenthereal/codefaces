db29d2407d2b17bcf35ee377af0810e9c76b25c7
Jingwen Owen Ou <jingweno@gmail.com>
2010-11-08 15:06:05 -0800

    fixed bug related to not being able to parse https github url.

--- org.codefaces.web.github.tests/src/org/codefaces/web/urls/github/GithubUrlParseStrategyTest.java
+++ org.codefaces.web.github.tests/src/org/codefaces/web/urls/github/GithubUrlParseStrategyTest.java
@@ -13,20 +13,21 @@ public class GithubUrlParseStrategyTest {
 
 	private static final String GITHUB_BRANCHES_DIR = "branches";
 
-	private static final String TEST_SCM_KIND = "GitHub";
+	private static final String SCM_KIND_GITHUB = "GitHub";
 
-	private static final String TEST_MASTER_FOLDER = GITHUB_BRANCHES_DIR
-			+ "/master";
+	private static final String MASTER_BRANCH = GITHUB_BRANCHES_DIR + "/master";
 
-	private static final String TEST_URL_MASTER_ROOT = "http://github.com/jingweno/ruby_grep";
+	private static final String HTTP_GITHUB_MASTER_BRANCH_URL = "http://github.com/jingweno/ruby_grep";
 
-	private static final String TEST_URL_MASTER_ROOT_WITH_TRAILING_SLASH = "http://github.com/jingweno/ruby_grep/";
+	private static final String HTTP_GITHUB_MASTER_BRANCH_URL_WITH_TRAILING_SLASH = "http://github.com/jingweno/ruby_grep/";
 
-	private static final String TEST_URL_MASTER_FILE = "http://github.com/jingweno/ruby_grep/blob/master/lib/ruby_grep.rb";
+	private static final String HTTP_GITHUB_MASTER_BRANCH_FILE_URL = "http://github.com/jingweno/ruby_grep/blob/master/lib/ruby_grep.rb";
 
-	private static final String TEST_URL_BRANCH_ROOT = "http://github.com/schacon/ruby-git/tree/internals";
+	private static final String HTTP_GITHUB_BRANCH_URL = "http://github.com/schacon/ruby-git/tree/internals";
 
-	private static final String TEST_URL_BRANCH_ROOT_WITH_TRAILING_SLASH = "http://github.com/schacon/ruby-git/tree/internals/";
+	private static final String HTTP_GITHUB_BRANCH_URL_WITH_TRAILING_SLASH = "http://github.com/schacon/ruby-git/tree/internals/";
+
+	private static final String HTTPS_GITHUB_MASTER_BRANCH_URL = "https://github.com/cowboyd/hudson.rb";
 
 	@Before
 	public void setUp() {
@@ -36,45 +37,49 @@ public class GithubUrlParseStrategyTest {
 	@Test
 	public void extractParameters_masterRoot() {
 		SCMURLConfiguration config = strategy
-				.buildConfigurations(TEST_URL_MASTER_ROOT);
+				.buildConfigurations(HTTP_GITHUB_MASTER_BRANCH_URL);
 
-		assertEquals("http://github.com/jingweno/ruby_grep",
+		assertEquals(HTTP_GITHUB_MASTER_BRANCH_URL,
 				config.get(SCMConfigurableElement.REPO_URL));
-		assertEquals(TEST_MASTER_FOLDER,
+		assertEquals(MASTER_BRANCH,
 				config.get(SCMConfigurableElement.BASE_DIRECTORY));
 	}
 
 	@Test
 	public void repositoryShouldBeParsedCorrectlyWhenMasterRootUrlContainsTrailingSlash() {
 		SCMURLConfiguration config = strategy
-				.buildConfigurations(TEST_URL_MASTER_ROOT_WITH_TRAILING_SLASH);
-		assertEquals("http://github.com/jingweno/ruby_grep",
+				.buildConfigurations(HTTP_GITHUB_MASTER_BRANCH_URL_WITH_TRAILING_SLASH);
+
+		assertEquals(HTTP_GITHUB_MASTER_BRANCH_URL,
 				config.get(SCMConfigurableElement.REPO_URL));
-		assertEquals(TEST_SCM_KIND, config.get(SCMConfigurableElement.SCM_KIND));
-		assertEquals(TEST_MASTER_FOLDER,
+		assertEquals(SCM_KIND_GITHUB,
+				config.get(SCMConfigurableElement.SCM_KIND));
+		assertEquals(MASTER_BRANCH,
 				config.get(SCMConfigurableElement.BASE_DIRECTORY));
 	}
 
 	@Test
 	public void extractParameters_masterFile() {
 		SCMURLConfiguration config = strategy
-				.buildConfigurations(TEST_URL_MASTER_FILE);
+				.buildConfigurations(HTTP_GITHUB_MASTER_BRANCH_FILE_URL);
 
-		assertEquals("http://github.com/jingweno/ruby_grep",
+		assertEquals(HTTP_GITHUB_MASTER_BRANCH_URL,
 				config.get(SCMConfigurableElement.REPO_URL));
-		assertEquals(TEST_SCM_KIND, config.get(SCMConfigurableElement.SCM_KIND));
-		assertEquals(GITHUB_BRANCHES_DIR + "/" + "master",
+		assertEquals(SCM_KIND_GITHUB,
+				config.get(SCMConfigurableElement.SCM_KIND));
+		assertEquals(MASTER_BRANCH,
 				config.get(SCMConfigurableElement.BASE_DIRECTORY));
 	}
 
 	@Test
 	public void extractParameters_branchRoot() {
 		SCMURLConfiguration config = strategy
-				.buildConfigurations(TEST_URL_BRANCH_ROOT);
+				.buildConfigurations(HTTP_GITHUB_BRANCH_URL);
 
 		assertEquals("http://github.com/schacon/ruby-git",
 				config.get(SCMConfigurableElement.REPO_URL));
-		assertEquals(TEST_SCM_KIND, config.get(SCMConfigurableElement.SCM_KIND));
+		assertEquals(SCM_KIND_GITHUB,
+				config.get(SCMConfigurableElement.SCM_KIND));
 		assertEquals(GITHUB_BRANCHES_DIR + "/" + "internals",
 				config.get(SCMConfigurableElement.BASE_DIRECTORY));
 	}
@@ -82,11 +87,31 @@ public class GithubUrlParseStrategyTest {
 	@Test
 	public void repositoryShouldBeParsedCorrectlyWhenBranchRootUrlContainsTrailingSlash() {
 		SCMURLConfiguration config = strategy
-				.buildConfigurations(TEST_URL_BRANCH_ROOT_WITH_TRAILING_SLASH);
+				.buildConfigurations(HTTP_GITHUB_BRANCH_URL_WITH_TRAILING_SLASH);
+
 		assertEquals("http://github.com/schacon/ruby-git",
 				config.get(SCMConfigurableElement.REPO_URL));
-		assertEquals(TEST_SCM_KIND, config.get(SCMConfigurableElement.SCM_KIND));
+		assertEquals(SCM_KIND_GITHUB,
+				config.get(SCMConfigurableElement.SCM_KIND));
 		assertEquals(GITHUB_BRANCHES_DIR + "/" + "internals",
+				config.get(SCMConfigurableElement.BASE_DIRECTORY));
+	}
+
+	@Test
+	public void canParseHTTPSGitHubMasterBranchURL() {
+		assertTrue(strategy.canParse(HTTPS_GITHUB_MASTER_BRANCH_URL));
+	}
+
+	@Test
+	public void buildConfigurationsForHTTPSGitHubMasterBranchURL() {
+		SCMURLConfiguration config = strategy
+				.buildConfigurations(HTTPS_GITHUB_MASTER_BRANCH_URL);
+
+		assertEquals(HTTPS_GITHUB_MASTER_BRANCH_URL,
+				config.get(SCMConfigurableElement.REPO_URL));
+		assertEquals(SCM_KIND_GITHUB,
+				config.get(SCMConfigurableElement.SCM_KIND));
+		assertEquals(MASTER_BRANCH,
 				config.get(SCMConfigurableElement.BASE_DIRECTORY));
 	}
 }
--- org.codefaces.web.github/src/org/codefaces/web/urls/github/GitHubUrlParseStrategy.java
+++ org.codefaces.web.github/src/org/codefaces/web/urls/github/GitHubUrlParseStrategy.java
@@ -8,24 +8,26 @@ import org.codefaces.ui.SCMURLConfiguration;
 import org.codefaces.web.urls.URLParsingStrategy;
 
 public class GitHubUrlParseStrategy implements URLParsingStrategy {
-	//TODO use GitHubConstants
+	// TODO use GitHubConstants
 	private static final String BRANCHES_FOLDER_NAME = "branches";
-	private static final String MASTER_BRANCH = BRANCHES_FOLDER_NAME + "/master";
-	
-	private static final String SCM_KIND = "GitHub";
-	
-	private static final String HTTP_WWW_GITHUB_ORG = "http://www.github.org";
+	private static final String MASTER_BRANCH = BRANCHES_FOLDER_NAME
+			+ "/master";
 
-	private static final String HTTP_GITHUB_COM = "http://github.com";
+	private static final String SCM_KIND = "GitHub";
+
+	private static final String HTTP_PREFIX = "http://";
+
+	private static final String HTTPS_PREFIX = "https://";
+
+	private static final String GITHUB_COM = "github.com";
 
 	private static final String OPTIONAL_ENDING_SLASH_PATTERN = "(?:/)?";
-	
+
 	private static final Pattern URL_PATTERN = Pattern.compile("((?:"
-			+ Pattern.quote(HTTP_WWW_GITHUB_ORG) + "|"
-			+ Pattern.quote(HTTP_GITHUB_COM)
+			+ Pattern.quote(HTTP_PREFIX + GITHUB_COM) + "|"
+			+ Pattern.quote(HTTPS_PREFIX + GITHUB_COM)
 			+ ")/[^/]+/[^/]+)(?:/[^/]+/([^/]+).*)?"
 			+ OPTIONAL_ENDING_SLASH_PATTERN);
-
 
 	@Override
 	public boolean canParse(String url) {
@@ -51,7 +53,7 @@ public class GitHubUrlParseStrategy implements URLParsingStrategy {
 			} else {
 				config.put(SCMConfigurableElement.BASE_DIRECTORY, MASTER_BRANCH);
 			}
-			
+
 			config.put(SCMConfigurableElement.SCM_KIND, SCM_KIND);
 		}
 
